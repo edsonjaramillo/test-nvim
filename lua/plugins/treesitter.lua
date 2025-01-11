c3dd6765e0e6713f2b3c5f7f718f7b8724d06afe
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-01-10 18:29:34 -0600

    feat; add lua to ensure_installed in treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "go" },
+            ensure_installed = { "go", "lua" },
             sync_install = false,
             highlight = { enable = true, },
             indent = { enable = true, },
