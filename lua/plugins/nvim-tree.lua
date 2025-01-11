17d7741380ec518d361975c0ace018c9fcb181a3
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-01-11 12:54:13 -0600

    feat: add nvim-tree.lua plugin for file exploration and configure key mappings

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,42 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        local nvimtree = require("nvim-tree")
+
+        nvimtree.setup({
+            hide_root_name = true,
+            view = {
+                width = 35,
+                relativenumber = true,
+            },
+            -- change folder arrow icons
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                },
+                icons = {
+                    glyphs = {
+                        folder = {
+                            arrow_closed = "", -- arrow when folder is closed
+                            arrow_open = "", -- arrow when folder is open
+                        },
+                    },
+                },
+            },
+            filters = {
+                custom = { ".git" },
+            },
+            git = {
+                enable = false,
+            },
+        })
+
+        -- for conciseness on setting keymaps
+        local keymap = vim
+            .keymap
+
+        -- open nvim-tree
+        keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    end
+}
