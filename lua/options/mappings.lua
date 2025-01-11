a52a31dab72d1247a4352f1a0d3582b1c8f5fa1f
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-01-11 12:55:15 -0600

    feat: improve clarity of and organization of mappings

--- lua/options/mappings.lua
+++ lua/options/mappings.lua
@@ -1,14 +1,25 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set termguicolors")
-vim.g.mapleader = " "
-
-local opt = vim.o
-opt.relativenumber = true
-
+-- Leader key configurations
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.o.mousemoveevent = true
+-- Indentation settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- UI settings
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+
+-- Disable built-in file explorer
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Mouse settings
+vim.opt.mousemoveevent = true
+
+-- split a new pane vertically using leader =
+vim.keymap.set("n", "<leader>=", "<C-w>v", { silent = true })
+-- split a new pane horizontally using leader -
+vim.keymap.set("n", "<leader>-", "<C-w>s", { silent = true })
